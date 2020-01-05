bac93980d53f73d2b552756aa690a5e710ea187d
difly <kryshen.yaroslav@gmail.com>
2020-01-06 01:24:15 +0200

    initial commit, create simple entity, dao

---
+++ app/src/main/java/org/difly/contactappkotlin/dao/ContactDao.kt
@@ -0,0 +1,22 @@
+package org.difly.contactappkotlin.dao
+
+import androidx.room.*
+import org.difly.contactappkotlin.entity.Contact
+
+@Dao
+interface ContactDao {
+    @Query("SELECT * from contact_table ORDER BY firstname ASC")
+    fun getAlphabetizedWords(): List<Contact>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(contact: Contact)
+
+    @Update
+    suspend fun update(contact: Contact)
+
+    @Delete
+    suspend fun delete(contact: Contact)
+
+    @Query("DELETE FROM contact_table")
+    suspend fun deleteAll()
+}
---
+++ app/src/main/java/org/difly/contactappkotlin/entity/Contact.kt
@@ -0,0 +1,22 @@
+package org.difly.contactappkotlin.entity
+
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "contact_table")
+class Contact(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    @ColumnInfo(name = "firstname")
+    @NonNull
+    val firstName: String,
+    @ColumnInfo(name = "lastname")
+    val  lastName: String,
+    @ColumnInfo(name = "phonetype")
+    val phoneType: String,
+    @ColumnInfo(name = "phonenumber")
+    @NonNull
+    val phoneNumber: String
+)
