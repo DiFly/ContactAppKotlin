863d2c6c19de123173e22bfada91a6db369cbb79
difly <kryshen.yaroslav@gmail.com>
2020-01-14 18:54:26 +0200

    add adapter to main activity

--- app/src/main/java/org/difly/contactappkotlin/MainActivity.kt
+++ app/src/main/java/org/difly/contactappkotlin/MainActivity.kt
@@ -1,12 +1,20 @@
 package org.difly.contactappkotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import org.difly.contactappkotlin.adapter.ContactListAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = ContactListAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
